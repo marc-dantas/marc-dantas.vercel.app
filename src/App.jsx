138532234214a8148563042bb16e5d5f6a85ded4
marc-dantas <marciodantasms@hotmail.com>
2023-05-14 14:52:11 -0300

    Projects update

--- src/App.jsx
+++ src/App.jsx
@@ -38,13 +38,12 @@ export default function App() {
           </Section>
           <Title>Work</Title>
           <Section title="Main projects">
-            <Block link="https://github.com/marc-dantas/pile/" title="Pile (work in progress)">Concatenative and stack-oriented language.</Block>
             <Block link="https://github.com/marc-dantas/racnul/" title="RaCNuL">RAndom Crazy NUmeration Language </Block>
             <Block link="https://github.com/marc-dantas/viztack/" title="Viztack">An educational website about reverse polish notation</Block>
-            <Block link="https://github.com/marc-dantas/cuper/" title="MExA">Custom parsing module written in Python.</Block>
             <Block link="https://gist.github.com/marc-dantas/171488bd960a5ddcc97c566510d77476" title="input.go">(Gist) Useful functions to get the input from the user in Golang </Block>
-            <Block link="https://github.com/marc-dantas/marc-dantas.vercel.app" title="marc-dantas.vercel.app">My website improved in React.js</Block>
+            <Block link="https://github.com/marc-dantas/marc-dantas.vercel.app" title="marc-dantas.vercel.app">My website improved in React.js (this website's source code)</Block>
             <Block link="https://github.com/marc-dantas/3d-py-pong" title="3d-py-pong">A 3D Pong game made in Python with Ursina Engine.</Block>
+            <Block link="https://github.com/marc-dantas/ts-list" title="TS List">A new version of an old todo list project written in TypeScript<Block/>
           </Section>
           <Title>Contact</Title>
           <Section title="Social media">
