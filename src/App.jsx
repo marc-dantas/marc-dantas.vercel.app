7c4d8c9b51ce76e68b22b965d144a542db176cba
marc-dantas <marciodantasms@hotmail.com>
2023-08-18 21:10:17 -0300

    simple changes

--- src/App.jsx
+++ src/App.jsx
@@ -25,30 +25,26 @@ export default function App() {
             <Button link="https://php.net">PHP</Button>
             <Button link="https://mysql.com">MySQL</Button>
             <Button link="https://rust-lang.org">Rust</Button>
-            <Button link="https://go.dev">Golang</Button>
-            <Button link="https://www.iso.org/standard/74528.html">C</Button>
           </Section>
           <Section title="Experience">
             <Button>Static web pages with React.js and Next.js</Button>
             <Button>Automation and Data Science Software with Python 3</Button>
             <Button>Dynamic web pages with PHP</Button>
             <Button>MySQL Databases knowledge</Button>
-            <Button>Terminal-based and embedded software with Rust and Golang</Button>
+            <Button>Terminal-based and embedded software with Rust</Button>
             <Button>Low-level knowledge in C and Assembly</Button>
           </Section>
           <Title>Work</Title>
           <Section title="Main projects">
-            <Block link="https://github.com/marc-dantas/racnul/" title="RaCNuL">RAndom Crazy NUmeration Language </Block>
+            <Block link="https://github.com/marc-dantas/pile/" title="RaCNuL">Esoteric, concatenative, stack-based, statically typed and compiled programming language for computers.</Block>
             <Block link="https://github.com/marc-dantas/viztack/" title="Viztack">An educational website about reverse polish notation</Block>
-            <Block link="https://gist.github.com/marc-dantas/171488bd960a5ddcc97c566510d77476" title="input.go">(Gist) Useful functions to get the input from the user in Golang </Block>
-            <Block link="https://github.com/marc-dantas/marc-dantas.vercel.app" title="marc-dantas.vercel.app">My website improved in React.js (this website's source code)</Block>
             <Block link="https://github.com/marc-dantas/3d-py-pong" title="3d-py-pong">A 3D Pong game made in Python with Ursina Engine.</Block>
-            <Block link="https://github.com/marc-dantas/ts-list" title="TS List">A new version of an old todo list project written in TypeScript</Block>
+            <Block link="https://github.com/marc-dantas/marc-dantas.vercel.app" title="marc-dantas.vercel.app">My website improved in React.js</Block>
           </Section>
           <Title>Contact</Title>
           <Section title="Social media">
             <Button link="https://github.com/marc-dantas">GitHub</Button>
-            <Button link="https://instagram.com/marciodantasms">Instagram</Button>
+            <Button link="https://x.com/marc_dantas">Twitter</Button>
             <Button link="mailto:marcio.dantas.pro@outlook.com">My E-Mail</Button>
           </Section>
         </div>
